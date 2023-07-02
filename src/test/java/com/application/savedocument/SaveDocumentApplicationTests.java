39061b8f66a9f6116fdb5b1925e77bee48c2f291
Semih <ibrahim.semih@gmail.com>
2023-07-02 21:44:12 +0300

    initial commit

---
+++ src/test/java/com/application/savedocument/SaveDocumentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.savedocument;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveDocumentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
